a8b709d015aa4f09fceb980719f8d4dc36e703d4
Emilia Dreamer <emilia@rymiel.space>
2022-02-11 04:21:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Any
+
+A nefarious little library to ""safely"" break the type system and perform type erasure in Crystal
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     any:
+       gitlab: rymiel/any
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "any"
+
+erased_string = Any.new "Hello, world!"
+puts erased_string.value(String) # => Hello, world!
+erased_string.value(Int32) # Throws at runtime!
+```
+
+## Contributing
+
+1. Fork it (<https://gitlab.com/rymiel/any/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Merge Request
+
+## Contributors
+
+- [Emilia Dreamer](https://gitlab.com/rymiel) - creator and maintainer
---
+++ src/any.cr
@@ -0,0 +1,53 @@
+struct Any
+  @@known_type_names = {} of Int32 => String
+  @object : Void*
+  @stored_type : Int32
+
+  def stored_type_name : String
+    @@known_type_names[@stored_type]
+  end
+
+  def initialize(obj : T) forall T
+    @object = Box.box obj
+    @stored_type = obj.crystal_type_id
+    @@known_type_names[@stored_type] = obj.class.name
+  end
+
+  # Retrieve the type-erased value, assuming it is of the given type `T`.
+  # Will throw if the stored value isn't of type `T` (or a subclass of it)
+  def value(t : T.class) : T forall T
+    {% begin %}
+    if t.crystal_instance_type_id == @stored_type
+      return Box(T).unbox @object
+    {% for i in T.all_subclasses %}
+    elsif {{i}}.crystal_instance_type_id == @stored_type
+      return Box({{ i }}).unbox @object
+    {% end %}
+    end
+    {% end %}
+    raise TypeCastError.new "This Any is holding an instance of #{stored_type_name}, not #{t}"
+  end
+
+  # Retrieve the type-erased value, assuming it is of the given type `T`.
+  # Returns null if the stored value isn't of type `T` (or a subclass of it)
+  def value?(t : T.class) : T? forall T
+    {% begin %}
+    if t.crystal_instance_type_id == @stored_type
+      return Box(T).unbox @object
+    {% for i in T.all_subclasses %}
+    elsif {{i}}.crystal_instance_type_id == @stored_type
+      return Box({{ i }}).unbox @object
+    {% end %}
+    end
+    {% end %}
+    nil
+  end
+
+  def inspect(io : IO)
+    io << "#<Any"
+    io << "(" << stored_type_name << ":" << @stored_type << "):"
+    @object.address.to_s(io, 16)
+    io << ">"
+  end
+end
+
